ab8045f3cf365170dbb1a630f126d35c1abba366
RafaelRay0 <rr1010ray@gmail.com>
2024-01-11 13:19:45 -0300

    Atualizei

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 
+
 function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+    let p = document.querySelector("p")
 
     if(min > max) {
 
-        alert("O valor min, tem quer ser menor!")
+        p.innerHTML = "Falha, tente novamente"
+        
     }
      else {
 
         const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-        alert(result)
+        p.innerHTML = result
      }   
 }
